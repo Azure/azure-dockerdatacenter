ee4e488ecf5d2358c276249c999414a858c69841
Dwai Banerjee <dwaiba@microsoft.com>
2016-10-01 14:20:48 +0200

    Update setup-ucp-dtr-node.sh

--- setup-ucp-dtr-node.sh
+++ setup-ucp-dtr-node.sh
@@ -22,8 +22,9 @@ curl --insecure https://$MASTERFQDN/ca > ca.pem
 FPRINT=$(openssl x509 -in ca.pem -noout -sha256 -fingerprint | awk -F= '{ print $2 }' )
 
 echo $(date) " - Loading docker install Tar"
-
-docker load < /opt/ucp/ucp-1.1.2_dtr-2.0.2.tar.gz
+cd /opt/ucp && wget https://packages.docker.com/caas/ucp-1.1.4_dtr-2.0.3.tar.gz
+#docker load < /opt/ucp/ucp-1.1.2_dtr-2.0.2.tar.gz
+docker load < /opt/ucp/ucp-1.1.4_dtr-2.0.3.tar.gz
 
 # Start installation of UCP and join agent Nodes to cluster
 
