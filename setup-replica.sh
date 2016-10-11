a053e2d9a55cd95c932874ab9e6b983dff9d76c9
Dwai Banerjee <dwaiba@microsoft.com>
2016-10-11 10:16:31 +0200

    Update setup-replica.sh

--- setup-replica.sh
+++ setup-replica.sh
@@ -50,9 +50,11 @@ echo $FPRINT
 # Load the predownloaded Tar File
 
 echo $(date) " - Loading docker install Tar"
-cd /opt/ucp && wget https://packages.docker.com/caas/ucp-1.1.4_dtr-2.0.3.tar.gz
+cd /opt/ucp && wget https://packages.docker.com/caas/ucp-2.0.0-beta1_dtr-2.1.0-beta1.tar.gz
+#cd /opt/ucp && wget https://packages.docker.com/caas/ucp-1.1.4_dtr-2.0.3.tar.gz
 #docker load < /opt/ucp/ucp-1.1.2_dtr-2.0.2.tar.gz
-docker load < /opt/ucp/ucp-1.1.4_dtr-2.0.3.tar.gz
+#docker load < /opt/ucp/ucp-1.1.4_dtr-2.0.3.tar.gz
+docker load < https://packages.docker.com/caas/ucp-2.0.0-beta1_dtr-2.1.0-beta1.tar.gz
 
 # Start installation of UCP and join Controller replica to master Controller
 
