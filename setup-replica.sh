95c69612ef30cfcb3b060cae63181c47ff0ef133
Dwai Banerjee <dwaiba@microsoft.com>
2016-10-12 10:42:00 +0200

    Update setup-replica.sh

--- setup-replica.sh
+++ setup-replica.sh
@@ -39,6 +39,14 @@ SendTimeout = 600
 SSLCiphers = "HIGH:!aNULL:!MD5"
 SSLProtocols = "SSLv3 TLSv1 TLSv1.1 TLSv1.2"'
 
+# System Update and docker version update
+DEBIAN_FRONTEND=noninteractiv apt-get update -y
+apt-get install -y apt-transport-https ca-certificates
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' >> /etc/apt/sources.list.d/docker.list
+apt-cache -y policy docker-engine
+DEBIAN_FRONTEND=noninteractiv apt-get update -y
+
 # Retrieve Fingerprint from Master Controller
 
 curl --insecure https://$MASTERFQDN/ca > ca.pem
