b4110ec3382cbd4e650fc1a93521ca29b49087d7
Dwai Banerjee <dwaiba@microsoft.com>
2016-10-12 20:06:11 +0200

    Update setup-replica.sh

--- setup-replica.sh
+++ setup-replica.sh
@@ -43,7 +43,8 @@ SSLProtocols = "SSLv3 TLSv1 TLSv1.1 TLSv1.2"'
 DEBIAN_FRONTEND=noninteractive apt-get -y update
 apt-get install -y apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' >> /etc/apt/sources.list.d/docker.list
+#echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' >> /etc/apt/sources.list.d/docker.list
+echo 'deb https://packages.docker.com/1.12/apt/repo ubuntu-trusty main' >> /etc/apt/sources.list.d/docker.list
 apt-cache policy docker-engine
 DEBIAN_FRONTEND=noninteractive apt-get -y update
 DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
