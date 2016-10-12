8dc334983c1a3147e2e2a1c748b13251132c5e70
Dwai Banerjee <dwaiba@microsoft.com>
2016-10-12 15:04:33 +0200

    Update setup-ucp-node.sh

--- setup-ucp-node.sh
+++ setup-ucp-node.sh
@@ -8,12 +8,13 @@ MASTERPRIVATEIP=$3
 SLEEP=$4
 
 # System Update and docker version update
-DEBIAN_FRONTEND=noninteractiv apt-get -y update
+DEBIAN_FRONTEND=noninteractive apt-get -y update
 apt-get install -y apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' >> /etc/apt/sources.list.d/docker.list
 apt-cache policy docker-engine
-DEBIAN_FRONTEND=noninteractiv apt-get -y update
+DEBIAN_FRONTEND=noninteractive apt-get -y update
+DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 
 # Implement delay timer to stagger joining of Agent Nodes to cluster
 
