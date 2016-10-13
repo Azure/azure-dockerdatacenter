4fb0407c7da4f688b9ac40ddd71a55185de5c161
Dwai Banerjee <dwaiba@microsoft.com>
2016-10-13 03:31:51 +0200

    Update setup-replica.sh

--- setup-replica.sh
+++ setup-replica.sh
@@ -51,40 +51,32 @@ DEBIAN_FRONTEND=noninteractive apt-get -y update
 DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 
 # Retrieve Fingerprint from Master Controller
-
-curl --insecure https://$MASTERFQDN/ca > ca.pem
-
-FPRINT=$(openssl x509 -in ca.pem -noout -sha256 -fingerprint | awk -F= '{ print $2 }' )
-
-echo $FPRINT
+#curl --insecure https://$MASTERFQDN/ca > ca.pem
+#FPRINT=$(openssl x509 -in ca.pem -noout -sha256 -fingerprint | awk -F= '{ print $2 }' )
+#echo $FPRINT
 
 # Load the downloaded Tar File
 
-echo $(date) " - Loading docker install Tar"
-cd /opt/ucp && wget https://packages.docker.com/caas/ucp-2.0.0-beta1_dtr-2.1.0-beta1.tar.gz
-#cd /opt/ucp && wget https://packages.docker.com/caas/ucp-1.1.4_dtr-2.0.3.tar.gz
-#docker load < /opt/ucp/ucp-1.1.2_dtr-2.0.2.tar.gz
-#docker load < /opt/ucp/ucp-1.1.4_dtr-2.0.3.tar.gz
-docker load < ucp-2.0.0-beta1_dtr-2.1.0-beta1.tar.gz
+#echo $(date) " - Loading docker install Tar"
+#cd /opt/ucp && wget https://packages.docker.com/caas/ucp-2.0.0-beta1_dtr-2.1.0-beta1.tar.gz
+#docker load < ucp-2.0.0-beta1_dtr-2.1.0-beta1.tar.gz
 
 # Start installation of UCP and join Controller replica to master Controller
 
-echo $(date) " - Loading complete.  Starting UCP Install of replica Controller"
+#docker run --rm -i \
+#    --name ucp \
+#    -v /var/run/docker.sock:/var/run/docker.sock \
+#    -e UCP_ADMIN_USER=admin \
+#    -e UCP_ADMIN_PASSWORD=$PASSWORD \
+#    docker/ucp:2.0.0-beta1 \
+#    join --replica --san $MASTERFQDN --url https://${MASTERFQDN}:443 --fingerprint "${FPRINT}"
 
-docker run --rm -i \
-    --name ucp \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -e UCP_ADMIN_USER=admin \
-    -e UCP_ADMIN_PASSWORD=$PASSWORD \
-    docker/ucp:2.0.0-beta1 \
-    join --replica --san $MASTERFQDN --url https://${MASTERFQDN}:443 --fingerprint "${FPRINT}"
-
-if [ $? -eq 0 ]
-then
- echo $(date) " - UCP installed and started on the master(replica) Controller"
-else
- echo $(date) " -- UCP installation failed"
-fi
+#if [ $? -eq 0 ]
+#then
+#echo $(date) " - UCP installed and started on the master(replica) Controller"
+#else
+# echo $(date) " -- UCP installation failed"
+#fi
 
 
 # Configure NginX for Interlock  
@@ -105,3 +97,18 @@ echo $(date) " - NginX and interlock config complete"
 else
  echo $(date) " -- NginX and interlock config failed"
 fi
+
+echo $(date) " - Staring Swarm Join as Manager to Leader UCP Controller"
+apt-get -y update && apt-get install -y curl jq
+# Create an environment variable with the user security token
+AUTHTOKEN=$(curl -sk -d '{"username":"admin","password":"'"$PASSWORD"'"}' https://10.2.0.6/auth/login | jq -r .auth_token)
+echo "$AUTHTOKEN"
+# Download the client certificate bundle
+curl -k -H "Authorization: Bearer ${AUTHTOKEN}" https://10.2.0.6/api/clientbundle -o bundle.zip
+unzip bundle.zip && chmod 755 env.sh && source env.sh
+docker swarm join-token manager|sed '1d'|sed '1d'|sed '$ d'>swarmjoin.sh
+unset DOCKER_TLS_VERIFY
+unset DOCKER_CERT_PATH
+unset DOCKER_HOST
+chmod 755 swarmjoin.sh
+source swarmjoin.sh
