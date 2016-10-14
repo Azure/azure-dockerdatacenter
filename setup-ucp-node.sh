c760f0d7803c279cde158fe2deb51cecea95c58e
Dwai Banerjee <dwaiba@microsoft.com>
2016-10-14 09:30:46 +0200

    Update setup-ucp-node.sh

--- setup-ucp-node.sh
+++ setup-ucp-node.sh
@@ -23,52 +23,6 @@ DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 echo $(date) "Sleeping for $SLEEP"
 sleep $SLEEP
 
-# Retrieve Fingerprint from Master Controller
-#curl --insecure https://$MASTERFQDN/ca > ca.pem
-#FPRINT=$(openssl x509 -in ca.pem -noout -sha256 -fingerprint | awk -F= '{ print $2 }' )
-#echo $FPRINT
-
-# Load the downloaded Tar File
-
-#echo $(date) " - Loading docker install Tar"
-#cd /opt/ucp && wget https://packages.docker.com/caas/ucp-2.0.0-beta1_dtr-2.1.0-beta1.tar.gz
-#docker load < ucp-2.0.0-beta1_dtr-2.1.0-beta1.tar.gz
-
-# Start installation of UCP and join agent Nodes to cluster
-#echo $(date) " - Loading complete.  Starting UCP Install of agent node"
-#docker run --rm -i \
-#    --name ucp \
-#    -v /var/run/docker.sock:/var/run/docker.sock \
-#    -e UCP_ADMIN_USER=admin \
-#    -e UCP_ADMIN_PASSWORD=$PASSWORD \
-#    docker/ucp:2.0.0-beta1 \
-#    join --san $MASTERFQDN --fresh-install --url https://${MASTERFQDN}:443 --fingerprint "${FPRINT}"
-
-#if [ $? -eq 0 ]
-#then
-# echo $(date) " - UCP installed and started on the agent node"
-#else
-#echo $(date) " -- UCP installation failed on agent node"
-#fi
-
-# Configure NginX
-
-echo $(date) " - Initiating NginX configuration on the agent node"
-
-docker run -d \
---label interlock.ext.name=nginx \
---restart=always \
--p 80:80 \
--p 443:443 \
-nginx \
-nginx -g "daemon off;" -c /etc/nginx/nginx.conf
-
-if [ $? -eq 0 ]
-then
- echo $(date) " - NginX Install complete"
-else
- echo $(date) " -- NginX Install failed"
-fi
 echo $(date) " - Staring Swarm Join as worker UCP Controller"
 apt-get -y update && apt-get install -y curl jq
 # Create an environment variable with the user security token
