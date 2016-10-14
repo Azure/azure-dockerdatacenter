f3c468ebc36c1490245d407226015746457bba3d
Dwai Banerjee <dwaiba@microsoft.com>
2016-10-14 09:51:35 +0200

    Update setup-ucp-dtr-node.sh

--- setup-ucp-dtr-node.sh
+++ setup-ucp-dtr-node.sh
@@ -26,102 +26,88 @@ apt-cache policy docker-engine
 DEBIAN_FRONTEND=noninteractive apt-get -y update
 DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 
-# Retrieve Fingerprint from Master Controller
+# Implement delay timer to stagger joining of Agent Nodes to cluster
 
-curl --insecure https://$MASTERFQDN/ca > ca.pem
-
-FPRINT=$(openssl x509 -in ca.pem -noout -sha256 -fingerprint | awk -F= '{ print $2 }' )
-
-echo $(date) " - Loading docker install Tar"
-cd /opt/ucp && wget https://packages.docker.com/caas/ucp-2.0.0-beta1_dtr-2.1.0-beta1.tar.gz
-#cd /opt/ucp && wget https://packages.docker.com/caas/ucp-1.1.4_dtr-2.0.3.tar.gz
-#docker load < /opt/ucp/ucp-1.1.2_dtr-2.0.2.tar.gz
-#docker load < /opt/ucp/ucp-1.1.4_dtr-2.0.3.tar.gz
-docker load < ucp-2.0.0-beta1_dtr-2.1.0-beta1.tar.gz
-
-# Start installation of UCP and join agent Nodes to cluster
-
-echo $(date) " - Loading complete.  Starting UCP Install of agent node"
-
-docker run --rm -i \
-    --name ucp \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -e UCP_ADMIN_USER=admin \
-    -e UCP_ADMIN_PASSWORD=$PASSWORD \
-    docker/ucp:2.0.0-beta1 \
-    join --san $MASTERFQDN --fresh-install --url https://${MASTERFQDN}:443 --fingerprint "${FPRINT}"
-
-if [ $? -eq 0 ]
-then
- echo $(date) " - UCP installed and started on the agent node"
-else
- echo $(date) " -- UCP installation failed"
-fi
-
-
-echo $(date) " - Setting up UCP Client Bundle"
-
-# Download the client certificate bundle
-
-AUTHTOKEN=$(curl -sk -d '{"username":"'$USER'","password":"'$PASSWORD'"}' https://$MASTERPRIVATEIP/auth/login | jq -r .auth_token)
-curl -k -H "Authorization: Bearer $AUTHTOKEN" https://$MASTERPRIVATEIP/api/clientbundle -o bundle.zip
-unzip -q bundle.zip
-source env.sh
-
-# Implementing delay before installing DTR
-
+joinucp() {
 echo $(date) "Sleeping for $SLEEP"
 sleep $SLEEP
+echo $(date) " - Staring Swarm Join as worker UCP Controller"
+apt-get -y update && apt-get install -y curl jq
+# Create an environment variable with the user security token
+AUTHTOKEN=$(curl -sk -d '{"username":"admin","password":"'"$PASSWORD"'"}' https://ucpclus0-ucpctrl/auth/login | jq -r .auth_token)
+echo "$AUTHTOKEN"
+# Download the client certificate bundle
+curl -k -H "Authorization: Bearer ${AUTHTOKEN}" https://ucpclus0-ucpctrl/api/clientbundle -o bundle.zip
+unzip bundle.zip && chmod 755 env.sh && source env.sh
+docker swarm join-token worker|sed '1d'|sed '1d'|sed '$ d'>swarmjoin.sh
+unset DOCKER_TLS_VERIFY
+unset DOCKER_CERT_PATH
+unset DOCKER_HOST
+chmod 755 swarmjoin.sh
+source swarmjoin.sh
+}
 
-# Get the certificates used by UCP
 
-echo $(date) " - Configuring DTR on master DTR node"
 
-curl -k $UCP_URL/ca > ucp-ca.pem
-
+joinucp;
 # Install DTR
-docker run -i --rm \
-  docker/dtr:2.1.0-beta1 install \
-  --ucp-url $UCP_URL \
+docker run -it --rm \
+  dockerhubenterprise/dtr:2.1.0-beta1 install \
   --ucp-node $UCP_NODE \
-  --dtr-external-url $DTR_PUBLIC_IP \
-  --ucp-username $USER --ucp-password $PASSWORD \
-  --replica-id $REPLICA_ID"0" \
-  --ucp-ca "$(cat ucp-ca.pem)"
+  --ucp-insecure-tls \
+  --dtr-external-url https://dlbpipddcdev01.westeurope.cloudapp.azure.com:443  \
+  --ucp-url https://ucpclus0-ucpctrl \
+  --ucp-username admin --ucp-password $PASSWORD \
+  --replica-id $REPLICA_ID"0" 
+ 
+ 
+ # Install DTR
+ #docker run -it --rm \
+ #  dockerhubenterprise/dtr:2.1.0-beta1 install \
+ #  --ucp-node $UCP_NODE \
+ #  --ucp-insecure-tls \
+ #  --dtr-external-url https://dlbpipddcdev01.westeurope.cloudapp.azure.com:443  \
+ #  --ucp-url https://ucpclus0-ucpctrl \
+ #  --ucp-username admin --ucp-password $PASSWORD \
+ # --replica-id $REPLICA_ID"0"
   
-if [ $? -eq 0 ]
-then
-echo $(date) " - Completed DTR installation on master DTR node"
-else
- echo $(date) " - DTR installation on master DTR node failed"
-fi
-
-
-for ((loop=1; loop<=$COUNT; loop++))
-do
-
-echo $(date) " - Start DTR installation on replica DTR node"  
-
-# Install DTR Replica
-docker run -i --rm \
-docker/dtr:2.1.0-beta1 join \
---ucp-url $UCP_URL \
---ucp-node $UCP_NODE_REP$loop$UCP_NODE_SUF \
---replica-id $REPLICA_ID$loop \
---existing-replica-id $REPLICA_ID"0" \
---ucp-username $USER --ucp-password $PASSWORD \
---ucp-ca "$(cat ucp-ca.pem)"
   
-if [ $? -eq 0 ]
-then
- echo $(date) " - Completed DTR installation on replica DTR node - $UCP_NODE_REP$loop$UCP_NODE_SUF"
-else
- echo $(date) " -- DTR installation on replica DTR node - $UCP_NODE_REP$loop$UCP_NODE_SUF failed"
-fi
+  
+ if [ $? -eq 0 ]
+ then
+ echo $(date) " - Completed DTR installation on master DTR node"
+ else
+  echo $(date) " - DTR installation on master DTR node failed"
+ fi
 
-
-sleep 20
-
-done
-
+ ##################
+ # WIP - TBC for BETA
+ ###########################
+ for ((loop=1; loop<=$COUNT; loop++))
+ do
+ 
+ echo $(date) " - Start DTR installation on replica DTR node"  
+ #joinucp()
+ # Install DTR Replica
+docker run -it --rm \
+dockerhubenterprise/dtr:2.1.0-beta1 join \
+ --ucp-url $UCP_URL \
+ --ucp-node $UCP_NODE_REP$loop$UCP_NODE_SUF \
+   --ucp-insecure-tls \
+ --replica-id $REPLICA_ID$loop \
+ --existing-replica-id $REPLICA_ID"0" \
+ --ucp-username $USER --ucp-password $PASSWORD 
+   
+ if [ $? -eq 0 ]
+ then
+  echo $(date) " - Completed DTR installation on replica DTR node - $UCP_NODE_REP$loop$UCP_NODE_SUF"
+ else
+  echo $(date) " -- DTR installation on replica DTR node - $UCP_NODE_REP$loop$UCP_NODE_SUF failed"
+ fi
+ 
+ 
+ sleep 20
+ 
+ done
+ 
 echo $(date) " - Completed DTR installation on Master and all replica DTR nodes"
