957588548dc73425dc43ec1425cc23cc820e706a
Dwai Banerjee <dwaiba@microsoft.com>
2016-10-15 15:35:52 +0200

    Update setup-controller.sh

--- setup-controller.sh
+++ setup-controller.sh
@@ -3,36 +3,10 @@ echo $(date) " - Starting Script"
 
 PASSWORD=$1
 MASTERFQDN=$2
-# Key not FILEURI - to be changed
+
 FILEURI=$3
 MASTERPRIVATEIP=$4
 
-#INTERLOCK_CONFIG='
-#ListenAddr = ":8080"
-#DockerURL = "'$MASTERPRIVATEIP':2376"
-#TLSCACert = "/certs/ca.pem"
-#TLSCert = "/certs/cert.pem"
-#TLSKey = "/certs/key.pem"
-#PollInterval = "2s"
-#[[Extensions]]
-#Name = "nginx"
-#ConfigPath = "/etc/nginx/nginx.conf"
-#PidPath = "/etc/nginx/nginx.pid"
-#MaxConn = 1024
-#Port = 80
-#SSLCertPath = ""
-#SSLCert = ""
-#SSLPort = 443
-#SSLOpts = ""
-#User = "www-data"
-#WorkerProcesses = 2
-#RLimitNoFile = 65535
-#ProxyConnectTimeout = 600
-#ProxySendTimeout = 600
-#ProxyReadTimeout = 600
-#SendTimeout = 600
-#SSLCiphers = "HIGH:!aNULL:!MD5"
-#SSLProtocols = "SSLv3 TLSv1 TLSv1.1 TLSv1.2"'
 
 #copy license key to /opt/ucp/ucp
 cat > /opt/ucp/docker_subscription.lic <<EOF
@@ -89,22 +63,3 @@ then
 else
  echo $(date) " -- UCP installation failed"
 fi
-
-# Configure NginX for Interlock  
-
-#echo $(date) " - Initiating NginX and interlock configuration on the master"
-
-#docker run -d \
-# -e INTERLOCK_CONFIG="$INTERLOCK_CONFIG" \
-# -v ucp-node-certs:/certs \
-# -p 8080:8080 \
-# --restart=always \
-# ehazlett/interlock:1.2.0 \
-# -D run
-
-#if [ $? -eq 0 ]
-#then
-# echo $(date) " -- NginX and interlock config complete"
-#else
-# echo $(date) " -- NEP om installation failed"
-#fi
